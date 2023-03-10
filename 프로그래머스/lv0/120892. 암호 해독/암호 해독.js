ece1f707a9258cec4cab24af0762a252176dacdb
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-03-10 23:31:49 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,10 +1,8 @@
 function solution(cipher, code) {
     var answer = '';
-    for(let i = 0; i <= cipher.length; i++){
-        if(i == code || i % code == 0) {
-            answer += cipher[i - 1];
-        }
+    for(let i = code; i <= cipher.length; i += code){
+        answer += cipher[i - 1];
     }
 
-    return answer.slice(9,answer.length);
+    return answer;
 }
