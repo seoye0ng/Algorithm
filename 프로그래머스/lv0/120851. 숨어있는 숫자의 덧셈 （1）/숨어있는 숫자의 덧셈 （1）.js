72b765d2a001393b55c06c837e86e31d2d903c21
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-03-12 22:54:39 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    var answer = 0;
+    
+    for (let i = 0; i < my_string.length; i++){
+        if(Number(my_string[i])){
+            answer += parseInt(my_string[i])
+        }
+    }
+
+    return answer;
+}
