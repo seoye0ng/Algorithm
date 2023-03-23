057403824ed767dc8b47a512da0efdc4e023ee36
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-03-23 23:54:13 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,5 +1,5 @@
 function solution(num, k) {
-    var answer = [...String(num)].findIndex(i => i == k);
+    var answer = String(num).indexOf(k);
      
     return answer !== -1 ? answer + 1 : answer;
 }
