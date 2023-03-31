831dec9bbd3858170bc7434c016fa2e6c45f907d
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-03-31 20:01:02 +0900

    [level 0] Title: 약수 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,10 +1,8 @@
 function solution(n) {
     var answer = [];
     
-    for(let i = 1; i <= n; i++){
-        if(n % i == 0){
-            answer.push(i)
-        }
+    for(let i = 1; i <= n; i++) {
+        if(n % i == 0) answer.push(i)
     }
     
     return answer;
