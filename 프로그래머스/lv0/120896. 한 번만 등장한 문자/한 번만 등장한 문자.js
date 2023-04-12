b036129fcc6b29aacf20531c7da89391d1fe37b3
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-04-13 00:32:51 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,6 +1,5 @@
 function solution(s) {
-    var arr = [...s];
-   
+    const arr = [...s];
     const repeatNums = arr.filter((v, i) => arr.indexOf(v) !== i)
     
     return arr.filter(item => !repeatNums.includes(item)).sort().join('');
