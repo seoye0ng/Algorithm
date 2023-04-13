6595c6474b6740be1b6398e6c0496abebd297595
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-04-14 00:03:01 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
     const sortArr = numbers.sort((a, b) => b - a);
    
-    const answer = sortArr[0] * sortArr[1] > sortArr[sortArr.length - 1] * sortArr[sortArr.length - 2] ? sortArr[0] * sortArr[1] : sortArr[sortArr.length - 1] * sortArr[sortArr.length - 2]
+    const answer = Math.max(sortArr[0] * sortArr[1], sortArr[sortArr.length - 1] * sortArr[sortArr.length - 2])
     return answer
 }
