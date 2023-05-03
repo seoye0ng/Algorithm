063c2dea004d6fbe9ba28381e6b2c1dc356cf4f1
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-05-03 23:52:58 +0900

    Update 삼각형의 완성조건 （2）.js

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,16 +1,13 @@
 function solution(sides) {
     let answer = 0;
-    let side1 = Math.max(...sides);
-    let side2 = Math.min(...sides);
-    
-    //  가장 긴 변이 이미 포함된 경우
-    for(let i = side1; i > 0; i--)
-        if(side1 < side2 + i && side1 >= i){
+    let [min, max] = [Math.min(...sides), Math.max(...sides)];
+   
+    for(let i = max; i > 0; i--)
+        if(max < min + i && max >= i){
             answer++;
         }
     
-    //  가장 긴 변인 경우
-    for(let i = side1 + 1; i < side1 + side2; i++){
+    for(let i = max + 1; i < max + min; i++){
         answer++;
     }
     
