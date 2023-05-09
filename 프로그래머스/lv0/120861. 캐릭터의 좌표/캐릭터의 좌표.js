6ec744b7925671b76579db0ff511050b234c86d2
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-05-09 16:35:35 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -4,15 +4,10 @@ function solution(keyinput, board) {
     const boardH =  Math.floor(board[1] / 2);
     
     keyinput.forEach((el) => {
-        if(el === 'up' && character[1] < boardH) { 
-            character[1] += 1
-        } else if(el === 'down' && character[1] > -boardH) {
-            character[1] -= 1 
-        } else if(el === 'right' && character[0] < boardW){ 
-            character[0] += 1
-        } else if(el === 'left' && character[0] > -boardW){
-            character[0] -= 1 
-        } 
+        if(el === 'up' && character[1] < boardH) character[1]++
+        else if(el === 'down' && character[1] > -boardH) character[1]--
+        else if(el === 'right' && character[0] < boardW) character[0]++
+        else if(el === 'left' && character[0] > -boardW) character[0]--
         })
     
     return character;
