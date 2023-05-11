bfec47a67f2c06e3b27fb2745d9b01eeb98500b2
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-05-12 00:54:16 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let arr = [];
+    
+    for(let i = 1; i < 1000; i++) {
+        if(i % 3 !== 0 && !i.toString().includes('3')) {
+            arr.push(i);
+        }
+    }
+    
+    return arr[n-1];
+}
