30db5ee009912665f513f5c2031dc9ba21daa6d5
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-05-15 23:46:12 +0900

    [level 0] Title: 외계어 사전, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,5 +1,3 @@
 function solution(spell, dic) {
-    var answer = 0;
-    
-    return dic.some(el => spell.every(v => el.includes(v))) ? 1 : 2
+   return dic.some(el => spell.every(val => el.includes(val))) ? 1 : 2
 }
