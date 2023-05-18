46f9afd2c95bf50d0bfb51fefb7b427167d76c24
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-05-18 23:04:34 +0900

    [level 0] Title: 등수 매기기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,15 @@
+function solution(score) {
+    let answer = [];
+    let average = []
+    
+    score.map(item => average.push((item[0]+item[1]) / 2))
+    
+    const arr = [...average].sort((a, b) => (b - a));
+    // 숫자가 큰 순서대로 정렬
+    for(let i in arr) {
+        answer.push(arr.indexOf(average[i]) + 1)
+         // 정렬된 값에 해당하는 값을 찾아서 인덱스는 0부터 시작하니까 +1을 해줌
+    }
+    
+    return answer;
+}
