62f8dbd13ca206d56664f3731bcda83c268b27e6
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-05-30 16:46:49 +0900

    [level 0] Title: 문자열 밀기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -5,10 +5,8 @@ function solution(A, B) {
         for(let i = 0; i < A.length; i++){
            const shifted = A.slice(-i) + A.slice(0, -i); 
             // A를 오른쪽으로 i칸 밀어낸 문자열
-           if (shifted === B) {
+           if (shifted === B) return i;
       // A를 밀어서 B와 일치하는 경우
-            return i;
-            } 
         } return -1
      }
 }
