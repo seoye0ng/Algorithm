250bbe38033dde62e8669b4c18ad89dd9b646684
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-08-09 23:59:30 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,8 +1,9 @@
 function solution(numbers) {
-    let numArr = numbers.sort()
     let answer = 0;
+    
     for(let i = 0; i <= 9; i++){
-        if(!numArr.includes(i)) answer += i
+        if(!numbers.includes(i)) answer += i
     }
+    
     return answer;
 }
