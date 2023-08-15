6269958e698ea4184d20877e113346a8b94eb409
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-08-15 23:53:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -2,7 +2,7 @@ function solution(n) {
     let answer = '';
     
     for(let i = 1; i <= n; i++){
-        i % 2 === 0 ? answer += '박' : answer += '수'
+        answer += i % 2 === 0 ? '박' : '수'
     }
     
     return answer;
