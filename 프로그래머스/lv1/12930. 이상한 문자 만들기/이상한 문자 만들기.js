df3bde39a556b06dd9822cdd167f3ddf5e2b9e6e
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-09-03 23:58:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,9 +1,7 @@
 function solution(s) {
     return s.split(' ').map((word) => {
-        let transformedWord = '';
-        for (let i = 0; i < word.length; i++) {
-            transformedWord += i % 2 === 0 ? word[i].toUpperCase() : word[i].toLowerCase();
-        }
-        return transformedWord;
-    }).join(' ');
+        return word.split('').map((char, i) => {
+            return i % 2 === 0 ? char.toUpperCase() : char.toLowerCase();
+        }).join('');
+    }).join(' '); 
 }
