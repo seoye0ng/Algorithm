60695066dcf6a08b56ace0cf486ba00546899076
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-09-04 00:14:20 +0900

    [level 1] Title: 행렬의 덧셈, Time: 17.95 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,6 @@
 function solution(arr1, arr2) {
-    let answer = [];
-    for(let i in arr1){
-        let row = []
-        for(let j in arr1[i]){
-            row.push(arr1[i][j] + arr2[i][j])
-        }
-        answer.push(row)
-    }
-    return answer
+    var answer = [];
+    arr1.map((i) => answer.push(i))
+ 
+    return arr1.map((i) => answer.push(i));
 }
