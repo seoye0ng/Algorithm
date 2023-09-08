47c14ebb546c4c5b3a11596b6c66ad499f2e0a16
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-09-08 19:12:13 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -2,7 +2,8 @@ function solution(t, p) {
     let answer = [];
     
     for(let i = 0; i <= t.length - p.length; i++){
-        if(t.slice(i, p.length + i) <= p) answer.push(t.slice(i, p.length + i))
+        const num = t.slice(i, p.length + i)
+        if(num <= p) answer.push(num)
     }
     
     return answer.length
