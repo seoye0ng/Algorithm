c2702449e0d80c3e89acc6f2663d9eacbca611d1
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-10-26 23:50:14 +0900

    [level 1] Title: 시저 암호, Time: 2.23 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,7 +1,7 @@
 function solution(s, n) {
-    let answer = s.split('').map((char) => {
+    const answer = s.split('').map((char) => {
         if (char !== ' ') {
-            let isLowerCase = char === char.toLowerCase();
+            let isLowerCase = char === char.toLowerCase(); // char의 소문자 여부를 나타내는 불리언
             let start = isLowerCase ? 'a'.charCodeAt(0) : 'A'.charCodeAt(0);
             let charCode = char.charCodeAt(0);
             let shiftedCharCode = (charCode - start + n) % 26 + start;
