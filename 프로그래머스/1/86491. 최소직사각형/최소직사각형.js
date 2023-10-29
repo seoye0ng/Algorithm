838e2ee77f0a1a4f2721ae733bd043c7a9a9febc
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-10-29 23:59:53 +0900

    [level 1] Title: 최소직사각형, Time: 6.35 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -2,11 +2,12 @@ function solution(sizes) {
     let maxWidth = 0;
     let maxHeight = 0;
 
-    for (let i = 0; i < sizes.length; i++) {
-        let card = sizes[i];
-        maxWidth = Math.max(maxWidth, Math.max(card[0], card[1]));
-        maxHeight = Math.max(maxHeight, Math.min(card[0], card[1]));
-    }
+    sizes.forEach(item => {
+        const [a, b] = item.sort((a, b) => b - a);
+        if (a > maxWidth) maxWidth = a;
+        if (b > maxHeight) maxHeight = b;
+    });
 
     return maxWidth * maxHeight;
 }
+
