2145a064aff936a2ac5335f8018d0be0491292fe
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-03 01:16:09 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -20,7 +20,23 @@ function solution(n, arr1, arr2) {
     return answer;
 }
 
+/* 비트 OR (|) 연산 / padStart / replaceAll
 
+** 비트 OR (|) 연산은 두 이진수를 비트 단위로 비교하여 결과를 생성. 이 연산은 각 비트를 비교하고, 두 비트 중 하나라도 1인 경우 해당 비트를 1로 설정하는 연산
+
+function solution2(n, arr1, arr2) {
+    const answer = [];
+    
+    for (let i = 0; i < n; i++) {
+        const combineBinaryArr = (arr1[i] | arr2[i]).toString(2);
+        const combineBinaryArrAll = combineBinaryArr.padStart(n, '0');
+        const row = combineBinaryArrAll.replaceAll("0", " ").replaceAll("1", "#");
+        answer.push(row);
+    }
+    
+    return answer;
+}
+*/
 
 
 
