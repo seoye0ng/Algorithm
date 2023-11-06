195313542220f1180578d79c52251f2120c5ed49
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-06 23:52:28 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,13 +1,22 @@
+// function solution(food) {
+//     let answer = [0];
+    
+//     for(let i = food.length - 1; i >= 0; i--) {
+//         const foodAmount = Math.floor(food[i] / 2);
+//         for(let j = 1; j <= foodAmount; j++) {
+//             answer.push(i)
+//             answer.unshift(i)
+//         }
+//     }
+    
+//     return answer.join('');
+// }
+
 function solution(food) {
-    let answer = [0];
-    
-    for(let i = food.length - 1; i >= 0; i--) {
-        const foodAmount = Math.floor(food[i] / 2);
-        for(let j = 1; j <= foodAmount; j++) {
-            answer.push(i)
-            answer.unshift(i)
-        }
+    let halfFood = '';
+    for (let i = 1; i < food.length; i++) {
+        halfFood += String(i).repeat(Math.floor(food[i] / 2));
     }
-    
-    return answer.join('');
+
+    return halfFood + '0' + [...halfFood].reverse().join('');
 }
