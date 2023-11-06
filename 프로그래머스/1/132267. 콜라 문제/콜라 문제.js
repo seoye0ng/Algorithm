29273d88154acc036e73203ff78ba7a685d6fbbd
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-07 00:42:41 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,11 +1,10 @@
 function solution(a, b, n) {
     let totalCola = 0;
-    let newCola = 0;
     
     while(n >= a){
-        newCola = Math.floor(n / a) * b;
+        const newCola = Math.floor(n / a) * b;
         totalCola += newCola;
-        n = n % a + newCola; // 남은 빈 병과 새로 받은 콜라를 이용하여 다음 계산
+        n = n % a + newCola;
     }
     
     return totalCola;
