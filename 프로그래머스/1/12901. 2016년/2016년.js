60d7dccf23db677ef2a2196d2e8a5222d618110e
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-07 14:48:20 +0900

    [level 1] Title: 2016년, Time: 0.14 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,11 +1,7 @@
 function solution(a, b) {
     const daysOfWeek = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
-    
-    const date = new Date;
-    date.setFullYear(2016);
-    date.setMonth(a - 1);
-    date.setDate(b);
-    
+
+    const date = new Date(2016, a - 1, b)
     
     return daysOfWeek[date.getDay()];
 }
