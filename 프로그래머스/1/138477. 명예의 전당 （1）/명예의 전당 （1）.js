e696f29fcefce6050fa90d304a2d1b2f982b72f1
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-07 13:47:29 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -2,18 +2,17 @@ function solution(k, score) {
     const answer = [];
     const highLevelScores = [];
     
-    for(let i = 0; i < score.length; i++){
+    for(let i = 0; i < score.length; i++) {
         let minScore = Math.min(...highLevelScores);
         
-        if(highLevelScores.length < k){
+        if(highLevelScores.length < k) {
             highLevelScores.push(score[i]);
         }else {
-            if (minScore < score[i]){
+            if (minScore < score[i]) {
                 highLevelScores.sort((a, b) => b - a);
                 highLevelScores.pop();
                 highLevelScores.push(score[i]);
             } 
-            
         }
         
         minScore = Math.min(...highLevelScores);    
