ce7c0b7441da2973b5c4cc872756bfa7dbbe94c4
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-10 02:07:39 +0900

    [level 1] Title: 소수 찾기, Time: 49.36 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,35 +1,21 @@
-// function solution(n) {
-//     if (n < 2) return 0; // 2 미만의 수는 소수가 아님
-
-//     let answer = 1;
-    
-//     for(let i = 3; i <= n; i += 2){
-//         if(isPrime(i)) answer++
-//     }
-    
-//     return answer;
-// }
-
-// function isPrime(num){
-//     const sqrtNum = Math.sqrt(num);
-//     for(let j = 2; j <= sqrtNum; j++){
-//         if(num % j === 0) return false
-//     }
-//     return true
-    
-// }
-
 function solution(n) {
+    // 에라토스테네스의 체를 위한 배열 초기화
     const sieve = Array(n + 1).fill(true);
+    
+    // 0과 1은 소수가 아니므로 false로 설정
     sieve[0] = sieve[1] = false;
 
+    // 2부터 n의 제곱근까지 반복
     for (let i = 2; i * i <= n; i++) {
+        // 현재 숫자가 소수인 경우
         if (sieve[i]) {
+            // 현재 숫자의 배수들을 모두 소수가 아닌 것으로 표시
             for (let j = i * i; j <= n; j += i) {
                 sieve[j] = false;
             }
         }
     }
 
+    // 배열에서 true인 값(소수)의 개수를 반환
     return sieve.filter((isPrime) => isPrime).length;
 }
