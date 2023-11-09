b9372b8483027b20b342c8ad15f8be9024d8b9cb
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-10 01:43:15 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,19 +1,19 @@
 function solution(nums) {
-    const answer = [];
+    const addNums = [];
 
-    // 서로 다른 세 수의 합을 구하여 answer 배열에 저장
+    // 서로 다른 세 수의 합을 구하여 addNums 배열에 저장
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             for (let k = j + 1; k < nums.length; k++) {
-                answer.push(nums[i] + nums[j] + nums[k]);
+                addNums.push(nums[i] + nums[j] + nums[k]);
             }
         }
     }
 
     // 소수 여부 판별 및 소수인 경우 카운트
     let count = 0;
-    for (let i = 0; i < answer.length; i++) {
-        if (isPrime(answer[i])) {
+    for (let i = 0; i < addNums.length; i++) {
+        if (isPrime(addNums[i])) {
             count++;
         }
     }
@@ -23,8 +23,8 @@ function solution(nums) {
 
 // 소수 판별 함수
 function isPrime(num) {
-    if (num < 2) return false;
-    for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num <= 2 || num % 2 === 0) return false;
+    for (let i = 3; i <= Math.sqrt(num); i++) {
         if (num % i === 0) return false;
     }
     return true;
