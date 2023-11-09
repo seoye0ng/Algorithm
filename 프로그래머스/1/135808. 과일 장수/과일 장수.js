70e7e562cd33cfd4e8cb1180ee4c1013be4d5b3d
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-09 23:54:36 +0900

    [level 1] Title: 과일 장수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,12 @@
+function solution(k, m, score) {
+    let answer = 0;
+    score.sort((a, b) => b - a);
+    
+    for(let i = m; i <= score.length; i = i + m){
+        answer += score[i - 1] * m;
+    }
+    
+    return answer;
+}
+
+    
