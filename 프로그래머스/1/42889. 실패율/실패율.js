3829004cbf52bbc5951233310639a3ccc8382530
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-14 17:05:26 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -9,6 +9,6 @@ function solution(N, stages) {
         answer.push([i, count / totalUser])
         totalUser = totalUser - count;
     }
-    
+//     각 원소의 실패율([1]) 순으로 정렬, map으로 각 원소의 [0]만 배열로 생성
     return answer.sort((a, b) => b[1] - a[1]).map(a => a[0]);
 }
