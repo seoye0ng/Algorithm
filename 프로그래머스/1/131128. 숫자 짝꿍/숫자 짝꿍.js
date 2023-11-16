bd95ebdf0b966e70bb75be7c7accb37149a56472
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-16 17:47:39 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -7,25 +7,20 @@ function solution(X, Y) {
     else if (duplicateNum.length === 1 && duplicateNum[0] === '0') return '0'
     else {
         const resultX = {}
-        const resultY = {}
         let matchStr = ''
     
         for(let i in duplicateNum) {
             X.split('').forEach(num => {
                 if (duplicateNum[i] === num) resultX[num] = (resultX[num] || 0) + 1; 
-                // 프로퍼티가 존재하지 않으면 0으로 초기화한 후 1을 더하기
                 }
             ) 
-            Y.split('').forEach(num => {
-                if (duplicateNum[i] === num) resultY[num] = (resultY[num] || 0) + 1;
-                }
-            )  
         }
     
         Object.entries(resultX).forEach(([key, value]) => {
-            if(resultY[key] === value) matchStr += key.repeat(value)
-            else if(resultY[key] >= value) matchStr += key.repeat(value)
-            else if(resultY[key] < value) matchStr += key.repeat(resultY[key])
+            const matchLength = Y.split('').filter(num => num === key).length;
+
+            if(matchLength <= value) matchStr += key.repeat(matchLength)
+            else  matchStr += key.repeat(value)
         })
         
         return matchStr.split('').sort((a,b) => b - a).join('')
