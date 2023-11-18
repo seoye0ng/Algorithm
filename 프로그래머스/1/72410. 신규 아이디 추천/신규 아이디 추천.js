0719a6ff5f85e51e8d0b10e89efcbaac3593d65c
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-18 15:03:08 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,34 +1,17 @@
 function solution(new_id) {
-    let answer = new_id.toLowerCase().replace(/[^a-z0-9.\-_]/g, '').split('');
-    
-    for(let i = 0; i < answer.length; i++){
-        if(i === 0 && answer[i] === '.') {
-            answer.shift()
-            i -= 1
-        } else {
-            if(answer[i] === '.' && answer[i + 1] === '.'){
-                answer.splice(i, 1)
-                i -= 1;
-            }
-        }
-        
-    }
-    if(answer.length > 15){
-        answer.splice(15, answer.length - 15)
-    }
-    else if(answer.length === 0){
-        answer = ['a', 'a', 'a']
-    }
-    if(answer[answer.length - 1] === '.'){
-         answer.pop()
-    }
-    if(answer.length < 3 && answer.length >= 1){
-         while(answer.length < 3 && answer.length >= 1){
-             answer.push(answer[answer.length - 1])
-         }
-    }
-    
-    
-    
-    return answer.join('');
+    let answer = new_id
+        .toLowerCase()
+        .replace(/[^a-z0-9.\-_]/g, '')
+        .replace(/\.{2,}/g, '.')
+        .replace(/^\.|\.$/g, '')
+        .slice(0, 15)
+        .padEnd(1, 'a')
+        .replace(/^\.|\.$/g, '')
+
+    return answer.padEnd(3, answer[answer.length - 1]);
 }
+// .replace(/\.{2,}/g, '.'): 연속된 마침표를 하나로 치환
+// .replace(/^\.|\.$/g, ''): 문자열의 시작이나 끝에 위치한 마침표를 제거
+// .padEnd(1, 'a'): 길이가 0인 경우에는 "a"를 추가
+// .slice(0, 15): 길이가 15를 초과하는 경우, 길이 15이상 제거
+// .padEnd(3, answer[answer.length - 1]): 길이가 3 미만인 경우, 마지막 문자로 패딩
