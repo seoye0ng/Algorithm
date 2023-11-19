888e1bb88cbd6994e1716c9025ab86136aa80a81
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-20 01:33:41 +0900

    [level 2] Title: 올바른 괄호, Time: 3.03 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,14 +1,11 @@
 function solution(s){
-    const stack = [];
+    let stack = 0;
 
     for(let i = 0; i < s.length; i++) {
-        if(s[i] === '(') {
-            stack.push(s[i])
-        } else if(s[i] === ')') {
-            if(stack.length > 0) stack.pop(); 
-            else return false; // s[0]이 ')'일 경우
-        }
+        if(s[i] === ')' && stack <= 0) return false;
+        if(s[i] === '(') stack++;
+        else stack--;
     }
     
-    return stack.length === 0;
+    return stack === 0;
 }
