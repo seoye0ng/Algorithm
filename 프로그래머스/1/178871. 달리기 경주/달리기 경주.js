004b25357d92d7d70760e4b3340c59441a72ae29
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-19 17:15:45 +0900

    [level 1] Title: 달리기 경주, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -2,16 +2,16 @@ function solution(players, callings) {
     const playerPositions = {};
     players.forEach((player, index) => {
         playerPositions[player] = index;
-    }); // 각 플레이어의 이름을 key로, 그 플레이어의 위치를 value로 저장
+    }); 
     
-    for(let i = 0; i < callings.length; i++){
-        const index = playerPositions[callings[i]];
+    callings.forEach(win => {
+        const index = playerPositions[win];
         if(index > 0) {
-            [players[index - 1], players[index]] = [players[index], players[index - 1]]; // 위치 변경
-            playerPositions[players[index - 1]] = index - 1;
+            [players[index - 1], players[index]] = [players[index], players[index - 1]];
             playerPositions[players[index]] = index;
+            playerPositions[players[index - 1]] = index - 1;
         }
-    }
-    
+    })
+        
     return players;
 }
