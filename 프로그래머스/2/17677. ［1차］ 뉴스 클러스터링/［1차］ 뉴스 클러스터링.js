772d7f3806dd52d671102f2bc095ef04caba33f0
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-24 01:34:36 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.42 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -0,0 +1,37 @@
+function solution(str1, str2) {
+    let intersection = 0;
+    let union = 0;
+    const str1Map = splitStr(str1);
+    const str2Map = splitStr(str2);
+    
+    str1Map.forEach((value, key) => {
+        if (str2Map.has(key)) {
+            intersection += Math.min(value, str2Map.get(key));
+            union += Math.max(value, str2Map.get(key));
+        } else {
+            union += value;
+        }
+    });
+  
+    str2Map.forEach((value, key) => {
+        if (!str1Map.has(key)) union += value;
+    });
+    
+    if (union === 0) return 65536;
+    return Math.floor((intersection / union) * 65536);
+}
+
+function splitStr(str) {
+    const regex = /^[a-zA-Z]+$/;
+    const map = new Map();
+
+    for(let i = 0; i < str.length - 1; i++){
+        if(regex.test(str[i]) && regex.test(str[i + 1])) {
+            const string = str[i].toUpperCase() + str[i + 1].toUpperCase();  
+            
+            if (map.has(string)) map.set(string, map.get(string) + 1);
+            else map.set(string, 1);
+        }
+    }
+    return map;
+}
