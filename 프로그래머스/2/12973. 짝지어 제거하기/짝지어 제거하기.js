61a91ca980576961930ecb68128516e8e8405773
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-11-23 23:06:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 29.77 ms, Memory: 47.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const stack = [];
+    
+    for(let i = 0; i < s.length; i++) {
+        if(i === 0 || stack[stack.length - 1] !== s[i]) {
+            stack.push(s[i]);
+        } else if(stack[stack.length - 1] === s[i]) {
+            stack.pop();
+        }
+    }
+    
+    return stack.length === 0 ? 1 : 0;
+}
