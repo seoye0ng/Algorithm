0259b5de4d202b847963e2b8d81e58afecb79bfa
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-12-01 00:30:13 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -2,23 +2,20 @@ function solution(number, k) {
     const stack = [];
     
     for (let i = 0; i < number.length; i++) {
-        const currentDigit = number[i];
-        
         // 스택에 있는 숫자 중 현재 숫자보다 작은 숫자를 모두 제거
-        while (k > 0 && stack.length > 0 && stack[stack.length - 1] < currentDigit) {
+        while (k > 0 && stack.length > 0 && stack[stack.length - 1] < number[i]) {
             stack.pop();
             k--;
         }
         
         // 현재 숫자를 스택에 추가
-        stack.push(currentDigit);
+        stack.push(number[i]);
     }
     
-    // 남은 자릿수 중에서 큰 숫자를 뒤에서부터 제거
     while (k > 0) {
         stack.pop();
         k--;
-    }
+    }    
     
     return stack.join('');
 }
