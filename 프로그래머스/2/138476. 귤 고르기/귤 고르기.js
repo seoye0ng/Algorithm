875830656c5a828deb4dca4a2e2bee7cd2b54080
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-12-08 15:11:28 +0900

    [level 2] Title: 귤 고르기, Time: 104.48 ms, Memory: 64.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,22 @@
+function solution(k, tangerine) {
+    let answer = 0;
+    const tangerineObj = {};
+    let temp = 0;
+    
+    for(let i = 0; i < tangerine.length; i++) {
+        if(tangerineObj[tangerine[i]]) tangerineObj[tangerine[i]] += 1;
+        else tangerineObj[tangerine[i]] = 1;
+    }
+    
+    const sortedObj = Object.entries(tangerineObj).sort((a, b) => b[1] - a[1]);
+
+    for(let [key, value] of sortedObj) {
+        if(temp >= k) break;
+        else {
+            temp += value;
+            answer++
+        };
+    }
+    
+    return answer;
+}
