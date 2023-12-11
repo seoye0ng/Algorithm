a25e1e4cf7dd30cc53cb9b31f14705c2adfdddcd
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-12-11 20:03:35 +0900

    [level 2] Title: 할인 행사, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,27 @@
+function solution(want, number, discount) {
+    let answer = 0;
+    const wantObj = {};
+    
+    for(let i = 0; i < want.length; i++) {
+        wantObj[want[i]] = number[i]
+    }
+    
+    for(let i = 0; i < discount.length - 9; i++) {
+        const temp = {...wantObj};
+        const slice = discount.slice(i, i + 10)
+        
+        for(let j = 0; j < slice.length; j++) {
+            if(temp[slice[j]] && temp[slice[j]] > 0) {
+                temp[slice[j]] -= 1;
+                if(j === slice.length - 1) {
+                    const check = Object.values(temp).every(value => value === 0);
+                    if(check) answer++;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}
