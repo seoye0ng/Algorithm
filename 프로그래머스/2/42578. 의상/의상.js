d1317fb4f3022fd0c574c7b9bc43197f4ad15675
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-12-12 23:49:56 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -11,8 +11,8 @@ function solution(clothes) {
     });
     
      for (let quantity of clothesObj.values()) {
-        answer *= quantity + 1;
+        answer *= quantity + 1; // 해당 카테고리의 옷을 입지 않는 경우를 고려하여 +1
     }
 
-    return answer - 1; 
+    return answer - 1; // 모든 카테고리의 옷을 입지 않는 경우를 빼줌
 }
