c9eac963300db94ebf3519475aa61a11f2c6c016
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-12-16 00:00:04 +0900

    [level 2] Title: 프로세스, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,20 @@
+function solution(priorities, location) {
+    let queue = priorities.map((priority, index) => ({ priority, index }));
+    let answer = 0;
+
+    while (queue.length > 0) {
+        let current = queue.shift();
+
+        // 현재 프로세스의 우선순위가 더 높은 프로세스가 있는지 확인
+        if (queue.some(process => process.priority > current.priority)) {
+            queue.push(current);
+        } else {
+            answer++;
+            if (current.index === location) {
+                break; // 찾고자 하는 프로세스가 실행될 때까지 반복
+            }
+        }
+    }
+
+    return answer;
+}
