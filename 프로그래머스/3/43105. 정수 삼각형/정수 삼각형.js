f6c14789bf5585b51b8c2a7f7fb5dea67ba82e8d
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-12-18 22:19:16 +0900

    [level 3] Title: 정수 삼각형, Time: 4.28 ms, Memory: 41.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,13 @@
+function solution(triangle) {
+    let answer = 0;
+    
+    for(let i = triangle.length - 1; i >= 0; i--) {
+        for(let j = 0; j < triangle[i].length - 1; j++) {
+            const temp = Math.max(triangle[i][j], triangle[i][j + 1]);
+            triangle[i - 1][j] += temp;
+        }   
+    }
+    
+    answer = triangle[0][0];
+    return answer;
+}
