8ecde2552bbd826269bd940e97cb8b73eec039c2
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-12-20 14:04:09 +0900

    [level 3] Title: 베스트앨범, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -4,27 +4,20 @@ function solution(genres, plays) {
     const playsObj = {};
     
     for(let i = 0; i < genres.length; i++) {
-        if(genresObj[genres[i]]) {
-            genresObj[genres[i]] += plays[i];
-        } else {
-            genresObj[genres[i]] = plays[i];
-        }
-        if(playsObj[genres[i]]) {
-            playsObj[genres[i]].push([plays[i] , i]);
-        } else {
-            playsObj[genres[i]] = [[plays[i] , i]];
-        }
+        if(genresObj[genres[i]]) genresObj[genres[i]] += plays[i];
+        else genresObj[genres[i]] = plays[i];
+        
+        if(playsObj[genres[i]]) playsObj[genres[i]].push([plays[i] , i]);
+        else playsObj[genres[i]] = [[plays[i] , i]];
     }
     
     const genresSort = Object.entries(genresObj).sort((a, b) => b[1] - a[1]);
     
     genresSort.forEach(music => {
         const playSort = playsObj[music[0]].sort((a, b) => b[0] - a[0]);
-        if(playsObj[music[0]].length === 1) {
-            answer.push(playsObj[music[0]][0][1])
-        } else {
-            answer.push(playsObj[music[0]][0][1])
-            answer.push(playsObj[music[0]][1][1])
+        for(let i = 0; i < 2; i++) {
+            answer.push(playsObj[music[0]][i][1])
+            if(playsObj[music[0]].length === 1) break;
         }
     })
 
