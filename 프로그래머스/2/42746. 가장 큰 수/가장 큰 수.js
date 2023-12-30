70ce3f9b6682d1334c77090e28e0912be9ab332f
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-12-31 00:03:00 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,11 @@
 function solution(numbers) {
     const sortedNumbers = numbers.map(num => num.toString())
                                   .sort((a, b) => (b + a) - (a + b));
+    /* sort 함수를 사용하여 문자열을 기반으로 정렬 
+    정렬 로직은 (b + a) - (a + b)를 기준으로 함
+    (b + a)는 문자열을 이어붙여서 만든 수를 나타내고, (a + b)는 다른 순서로 이어붙여 만든 수를 나타냄
+    이 둘을 비교하여 정렬하면, 큰 수가 앞에 위치하도록 정렬
+    예를 들어, b=3, a=30이라고 하면, (b + a)는 "330", (a + b)는 "303"이 되고, "330"이 "303"보다 크므로 "330"이 앞으로 정렬 */
     
     // 모든 숫자가 0인 경우 '0'으로 반환
     if (sortedNumbers[0] === '0') {
