d6e2e0491d30c4aff0d9f6c796642279b0849a61
백서영 <101791501+seoye0ng@users.noreply.github.com>
2024-03-30 18:08:09 +0900

    [level 1] Title: 모의고사, Time: 3.05 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,27 +1,25 @@
 function solution(answers) {
-    const patterns = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ];
+    const patterns = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]];
+    let answer = [];
+    let maxScore = 0;
     
-    const scores = [0, 0, 0];
-    
-    answers.forEach((answer, index) => {
-        patterns.forEach((pattern, idx) => {
-            if (answer === pattern[index % pattern.length]) {
-                scores[idx]++;
+    patterns.forEach((pattern, i) => {
+        let score = 0;
+        
+        answers.forEach((answer, idx) => {
+            if(answer === pattern[idx % pattern.length]) {
+                score++;
             }
-        });
-    });
-    
-    const maxScore = Math.max(...scores);
-    const result = scores.reduce((acc, score, index) => {
-        if (score === maxScore) {
-            acc.push(index + 1);
+        })
+        
+        if(score > maxScore) {
+            answer = [i + 1];
+            maxScore = score;
+        } else if(score === maxScore) {
+            answer.push(i + 1);
         }
-        return acc;
-    }, []);
+    })
     
-    return result;
+    return answer;
 }
+
