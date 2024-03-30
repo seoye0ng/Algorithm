77f44ee00960c541a8b6072fb29adf62d4d85fd2
백서영 <101791501+seoye0ng@users.noreply.github.com>
2024-03-30 18:37:41 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,20 +1,22 @@
 function solution(operations) {
-    let queue = [];
-
-    for (let operation of operations) {
-        let [command, num] = operation.split(' ');
-        num = Number(num);
-
-        if (command === 'I') {
+    let answer = [];
+    const queue = [];
+    
+    for(let i = 0; i < operations.length; i++) {
+        let [command, num] = operations[i].split(' ');
+        num = parseInt(num);
+        
+        if(command === 'I') {
             queue.push(num);
-        } else if (command === 'D' && queue.length > 0) {
-            if (num === 1) {
-                queue.splice(queue.indexOf(Math.max(...queue)), 1);
-            } else if (num === -1) {
-                queue.splice(queue.indexOf(Math.min(...queue)), 1);
+        } else if(command === 'D' && queue.length > 0){
+            if(num === -1) {
+                queue.sort((a, b) => b - a);
+            } else if(num === 1) {
+                queue.sort((a, b) => a - b);
             }
+            queue.pop();
         }
     }
-
+    
     return queue.length === 0 ? [0, 0] : [Math.max(...queue), Math.min(...queue)];
 }
