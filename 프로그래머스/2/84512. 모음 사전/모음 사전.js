6805e848e37864f3b980606391dd89278e98eb94
백서영 <101791501+seoye0ng@users.noreply.github.com>
2024-03-30 19:57:02 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,13 @@
+function solution(word) {
+    const vowels = ['A', 'E', 'I', 'O', 'U'];
+    let answer = 0;
+    let per = 781; 
+    // 5자리수에서 시작할 때, 각 자리수가 바뀔 때마다 증가하는 수 (5^4 + 5^3 + 5^2 + 5^1) + 각 자리수가 바뀔 때마다 증가하는 수의 시작점(1)
+
+    for (let i = 0; i < word.length; i++) {
+        answer += vowels.indexOf(word[i]) * per + 1;
+        per = (per - 1) / 5; // 다음 자리수로 넘어갈 때, 증가하는 수 조정
+    }
+
+    return answer;
+}
