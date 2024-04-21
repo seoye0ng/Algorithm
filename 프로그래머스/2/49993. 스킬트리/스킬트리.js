d3b79cf170b6a7d15132c32a830096445b20d0be
백서영 <101791501+seoye0ng@users.noreply.github.com>
2024-04-21 22:13:02 +0900

    [level 2] Title: 스킬트리, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -10,14 +10,13 @@ function solution(skill, skill_trees) {
         // 현재 스킬트리에서 각 스킬이 선행 스킬 순서를 지키는지 검사
         for (let j = 0; j < skill_trees[i].length; j++) {
             if (skill.includes(skill_trees[i][j])) {
-                // 현재 스킬이 선행 스킬 순서에 있으면 순서가 맞는지 검사
-                if (skill[index] === skill_trees[i][j]) {
-                    index++;
-                } else {
-                    // 순서가 맞지 않으면 불가능한 스킬트리로 판단
+                // 순서가 맞지 않으면 불가능한 스킬트리로 판단
+                if (skill[index] !== skill_trees[i][j]) {
                     valid = false;
                     break;
                 }
+                // 현재 스킬이 선행 스킬 순서가 맞다면 다음순서로
+                index++;
             }
         }
         
