3d2b5574e19bc26b07da514370d3da9ffe7e39da
백서영 <101791501+seoye0ng@users.noreply.github.com>
2024-04-29 22:21:47 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.51 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,16 +1,14 @@
 function solution(x, y, n) {
     const queue = [[y, 0]]; 
     
-    while (queue.length > 0) {
+    while (queue.length) {
         const [current, count] = queue.shift();
         
-        if (current === x) {
-            return count;
-        }
+        if (current === x) return count;
         
         // 2로 나누어 떨어질 때 값을 큐에 저장
         if(current % 2 === 0 && current / 2 >= x) {
-            queue.push([current/2, count + 1]);
+            queue.push([current / 2, count + 1]);
         }
         // 3로 나누어 떨어질 때 값을 큐에 저장
         if(current % 3 === 0 && current / 3 >= x) {
