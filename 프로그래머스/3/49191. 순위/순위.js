78f3a21a214f149383b0536b3c0ae8f5d687870b
백서영 <101791501+seoye0ng@users.noreply.github.com>
2024-06-26 00:00:30 +0900

    [level 3] Title: 순위, Time: 11.51 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,10 +1,13 @@
 function solution(n, results) {
+    // 그래프 초기화
     const win = Array.from({ length: n + 1 }, () => Array(n + 1).fill(false));
     
+    // 주어진 결과로 그래프 채우기
     results.forEach(([winner, loser]) => {
         win[winner][loser] = true;
     });
     
+    // 플로이드-와샬 알고리즘 적용하여 간접 승리 관계 찾기
     for (let k = 1; k <= n; k++) {
         for (let i = 1; i <= n; i++) {
             for (let j = 1; j <= n; j++) {
@@ -15,6 +18,7 @@ function solution(n, results) {
         }
     }
     
+    // 정확한 순위를 가질 수 있는 선수의 수를 결정
     let count = 0;
     for (let i = 1; i <= n; i++) {
         let definiteRank = true;
