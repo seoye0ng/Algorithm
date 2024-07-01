413a855fb28df77be9b9d3210df7e2e79f2c3124
백서영 <101791501+seoye0ng@users.noreply.github.com>
2024-07-01 23:36:23 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,13 +1,29 @@
-function solution(brown, yellow) {
-    let answer = [];
-    const total = brown + yellow;
+// function solution(brown, yellow) {
+//     let answer = [];
+//     const total = brown + yellow;
      
-    for(let i = 3;  i <= total / 2; i++) {
-        const row = total / i;
-        if(total % i === 0 && row >= i) {
-            if((row - 2) * (i - 2) === yellow) answer.push(row, i);
+//     for(let i = 3;  i <= total / 2; i++) {
+//         const row = total / i;
+//         if(total % i === 0 && row >= i) {
+//             if((row - 2) * (i - 2) === yellow) answer.push(row, i);
+//         }
+//     }
+    
+//     return answer;
+// }
+
+function solution(brown, yellow) {
+    const total_tiles = brown + yellow;
+
+    for (let width = 3; width <= total_tiles; width++) {
+        if (total_tiles % width === 0) {
+            let height = total_tiles / width;
+            if (width >= height) {
+                let yellow_tiles = (width - 2) * (height - 2);
+                if (yellow_tiles === yellow) {
+                    return [width, height];
+                }
+            }
         }
     }
-    
-    return answer;
 }
